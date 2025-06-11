351f74e0da4a6dc66863b11c1ba10e7486da286c
Parsa <parsam280xyz@gmail.com>
2025-06-11 18:18:01 +0000

    Initial Commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,5 @@
+return {
+    {'nvim-lualine/lualine.nvim',dependencies = { 'nvim-tree/nvim-web-devicons' }},{'nvim-telescope/telescope.nvim', tag = '0.1.8',dependencies = { 'nvim-lua/plenary.nvim' }}
+}
+
+
